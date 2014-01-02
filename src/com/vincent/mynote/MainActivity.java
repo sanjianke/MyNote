141ffd47b0228eef16e470dc9058dff29bc27a64
vincent <du_sunboy@qq.com>
2014-01-03 01:09:55 +0800

    add

--- src/com/vincent/mynote/MainActivity.java
+++ src/com/vincent/mynote/MainActivity.java
@@ -1,24 +1,18 @@
 package com.vincent.mynote;
 
-import com.vincent.mynote.base.BaseFramentActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class MainActivity extends BaseFramentActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//aaaaaaaaaaaa
 		
 		
-	}
-	
-	private void ahah() {
-		// TODO Auto-generated method stub
-		System.out.println("saaaaaaaa");
 	}
 
 	@Override
--- src/com/vincent/mynote/base/BaseFramentActivity.java
+++
@@ -1,7 +0,0 @@
-package com.vincent.mynote.base;
-
-import android.app.Activity;
-
-public class BaseFramentActivity extends Activity{
-
-}
